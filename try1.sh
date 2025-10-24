7dbe3b77d590bbe5203ce6ccfbeab0e4f7252167
Jesse-Barnum <103386126+Jesse-Barnum@users.noreply.github.com>
2025-10-24 17:23:50 -0600

    Update and rename try.sh to try1.sh

--- try1.sh
+++ try1.sh
@@ -16,7 +16,7 @@ sudo systemctl enable --now clamav-freshclam clamav-daemon
 
     # Install Wazuh Agent
     echo "Installing Wazuh Agent..."
-
+WAZUH_MANAGER_IP="10.0.0.5"
     curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | gpg --no-default-keyring --keyring gnupg-ring:/usr/share/keyrings/wazuh.gpg --import && chmod 644 /usr/share/keyrings/wazuh.gpg
     echo "deb [signed-by=/usr/share/keyrings/wazuh.gpg] https://packages.wazuh.com/4.x/apt/ stable main" | tee -a /etc/apt/sources.list.d/wazuh.list
     apt-get update
