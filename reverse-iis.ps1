4814c55fb371cf9397d00cc976022955e654d892
Jesse-Barnum <103386126+Jesse-Barnum@users.noreply.github.com>
2026-01-16 20:30:32 -0700

    Update reverse-iis.ps1

--- reverse-iis.ps1
+++ reverse-iis.ps1
@@ -11,19 +11,4 @@ Remove-WebConfigurationProperty -Filter /system.webServer/security/requestFilter
 Remove-WebConfigurationProperty -Filter /system.webServer/security/requestFiltering/fileExtensions -Name . -AtElement @{fileExtension='.cmd'}
 Remove-WebConfigurationProperty -Filter /system.webServer/security/requestFiltering/fileExtensions -Name . -AtElement @{fileExtension='.ps1'}
 
-# 3. Unblock "bin" Folder access
-Remove-WebConfigurationProperty -Filter /system.webServer/security/requestFiltering/hiddenSegments -Name . -AtElement @{segment='bin'}
-
-# 4. Re-Enable Directory Browsing (Make files listable again)
 Set-WebConfigurationProperty -Filter /system.webServer/directoryBrowse -Name enabled -Value True
-
-# 5. Restore Server Headers (Stop hiding identity)
-# Re-enable the "Server" header
-Set-WebConfigurationProperty -Filter /system.webServer/security/requestFiltering -Name removeServerHeader -Value False
-
-# Note: Putting "X-Powered-By" back is harder because it's a default, 
-# but simply removing the block rule usually suffices. 
-# If you explicitly removed it, this command adds it back:
-Add-WebConfigurationProperty -Filter /system.webServer/httpProtocol/customHeaders -Name . -Value @{name='X-Powered-By';value='ASP.NET'}
-
-Write-Host "IIS Hardening Reverted. Verify services are running!" -ForegroundColor Green
