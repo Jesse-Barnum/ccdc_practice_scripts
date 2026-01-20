7e9bd7f650250396a0f611cfdc46aba604589e54
Jesse-Barnum <103386126+Jesse-Barnum@users.noreply.github.com>
2026-01-19 21:06:35 -0700

    Update clam.ps1

--- clam.ps1
+++ clam.ps1
@@ -14,21 +14,3 @@ Register-ScheduledTask -TaskName "ClamAV Hourly Scan" -Action $action -Trigger $
 Start-Service -Name "ClamAV ClamD"
 Start-Service -Name "ClamAV FreshClam"
 
-# --- Step 3: Install and Configure Wazuh Agent ---
-Write-Host "Installing Wazuh Agent..."
-# 1. This is the installation command from the PDF
-msiexec /i ".\wazuh-agent-4.7.3.msi" WAZUH_MANAGER="10.0.0.5" WAZUH_REGISTRATION_PASSWORD="MyStrongKey" /qn
-
-# 2. NECESSARY commands to enable the modules from the PDF
-Write-Host "Configuring Wazuh Agent..."
-$wazuhConfigFile = "C:\Program Files (x86)\ossec-agent\ossec.conf"
-$configContent = Get-Content -Path $wazuhConfigFile -Raw
-$configContent = $configContent -replace "<syscollector><disabled>yes</disabled></syscollector>", "<syscollector><disabled>no</disabled></syscollector>"
-$configContent = $configContent -replace '<fim><disabled>yes</disabled></fim>', '<fim><disabled>no</disabled><directories realtime="yes">C:\Users</directories></fim>'
-Set-Content -Path $wazuhConfigFile -Value $configContent
-
-# 3. NECESSARY command, as implied by "Don't forget to restart"
-Write-Host "Restarting Wazuh Agent..."
-Restart-Service -Name "Wazuh"
-
-Write-Host "--- Script Complete ---"
