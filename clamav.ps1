679980e77434818e64d2c71f8cde3d0efdaf9290
Jesse-Barnum <103386126+Jesse-Barnum@users.noreply.github.com>
2026-02-09 12:46:04 -0700

    Update clamav.ps1

--- clamav.ps1
+++ clamav.ps1
@@ -8,18 +8,7 @@ copy .\conf_examples\clamd.conf.sample .\clamd.conf
 (Get-Content "C:\Program Files\ClamAV\freshclam.conf") -replace '^Example', '#Example' | Set-Content "C:\Program Files\ClamAV\freshclam.conf"
 (Get-Content "C:\Program Files\ClamAV\clamd.conf") -replace '^Example', '#Example' | Set-Content "C:\Program Files\ClamAV\clamd.conf"
 
-#update the database
-#.\freshclam.exe
-
 # Create a Scheduled Task for ClamAV to scan every 30 minutes
-schtasks /create /tn "Clam_30min_scan" /tr "\"C:\Program Files\ClamAV\clamscan.exe\" -r -i --log=\"C:\temp\System_scan.log\" \"C:\System32\"" /sc minute /mo 30 /ru System /rl highest
+schtasks /create /tn "Clam_30min_scan" /tr "\"C:\Program Files\ClamAV\clamscan.exe\" -r -i --log=\"C:\temp\System_scan.log\" \"C:\Windows\System32\"" /sc minute /mo 30 /ru System /rl highest
 
 
-
-
-#if the ./freshclam failed:
-#setx CURL_CA_BUNDLE "C:\Program Files\ClamAV\cacert.pem" /m
-# Download these three files (you can do this in the browser shown in your screenshots):
-# main.cvd
-# daily.
-# bytecode.cvd
