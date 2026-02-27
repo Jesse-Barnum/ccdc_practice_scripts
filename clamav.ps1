54711c66f5c6e6051f5306946a5b669acc84b7c3
Jesse-Barnum <103386126+Jesse-Barnum@users.noreply.github.com>
2026-02-26 17:22:57 -0700

    Update ClamAV configuration and remove scheduled task
    Remove scheduled task creation for ClamAV scans and update configuration files.

--- clamav.ps1
+++ clamav.ps1
@@ -7,6 +7,3 @@ copy .\conf_examples\clamd.conf.sample .\clamd.conf
 #replace the 'example' portion of the conf files so they operate correctly.
 (Get-Content "C:\Program Files\ClamAV\freshclam.conf") -replace '^Example', '#Example' | Set-Content "C:\Program Files\ClamAV\freshclam.conf"
 (Get-Content "C:\Program Files\ClamAV\clamd.conf") -replace '^Example', '#Example' | Set-Content "C:\Program Files\ClamAV\clamd.conf"
-
-# Create a Scheduled Task for ClamAV to scan every 30 minutes
-schtasks /create /tn "Clam_30min_scan" /tr " \"C:\Program Files\ClamAV\clamscan.exe\" -r -i --log=\"C:\temp\System_scan.log\" \"C:\Windows\System32\" " /sc minute /mo 30 /ru System /rl highest /f
